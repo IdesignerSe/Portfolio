7a009d70a3069e42fd3c96570691707f404f8462
idesignerSe <hebera75@gmail.com>
2026-06-04 15:09:45 +0200

    rendering test

--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -1,18 +1,18 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-// TypeScript can't find declaration for the JS component; silence the error
-// @ts-ignore
-import SciFiDesk from "./components/SciFiDesk/SciFiDesk";
 
 export default function ThreeScene() {
   return (
     <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
-      <ambientLight intensity={0.4} />
+      <ambientLight intensity={1} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
 
-      <OrbitControls enableZoom={false} />
+      <OrbitControls />
 
-      <SciFiDesk />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
     </Canvas>
   );
 }
--- src/components/SciFiDesk/SciFiDesk.tsx
+++ src/components/SciFiDesk/SciFiDesk.tsx
@@ -1,16 +1,29 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import SciFiDesk from "./SciFiDesk";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
-export default function ThreeScene() {
+export default function SciFiDesk() {
+  const deskRef = useRef<THREE.Group>(null!);
+
+  useFrame(() => {
+    if (deskRef.current) {
+      deskRef.current.rotation.y += 0.01;
+    }
+  });
+
   return (
-    <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
-      <ambientLight intensity={0.4} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+    <group ref={deskRef}>
+      {/* Desk base */}
+      <mesh position={[0, -1, 0]}>
+        <boxGeometry args={[3, 0.2, 1.5]} />
+        <meshStandardMaterial color="#0a0a0f" emissive="#1a1aff" emissiveIntensity={0.3} />
+      </mesh>
 
-      <OrbitControls enableZoom={false} />
-
-      <SciFiDesk />
-    </Canvas>
+      {/* Hologram projector */}
+      <mesh position={[0, -0.8, 0]}>
+        <cylinderGeometry args={[0.2, 0.2, 0.1, 32]} />
+        <meshStandardMaterial color="#111" emissive="#00aaff" emissiveIntensity={1} />
+      </mesh>
+    </group>
   );
 }

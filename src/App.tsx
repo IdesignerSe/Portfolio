643af419a7ed439944ee3241ff3a1f1fee8b294d
idesignerSe <hebera75@gmail.com>
2026-06-15 11:46:03 +0200

    Start Over

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-
-// @ts-ignore: importing a JS/JSX module without a TypeScript declaration file
-import ThreeScene from "./ThreeScene.jsx";
-
-export default function App() {
-  return <ThreeScene />;
-}

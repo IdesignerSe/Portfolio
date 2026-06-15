a6d5ab6036f68a4399f421901d5ab2d592653286
idesignerSe <hebera75@gmail.com>
2026-06-15 14:04:35 +0200

    Mobile First set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Photography from "./pages/Photography";
+import Blogg from "./pages/Blogg";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <div>
+    <BrowserRouter basename="/portfolio">
       <Navbar />
-      <Hero />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/photography" element={<Photography />} />
+        <Route path="/blogg" element={<Blogg />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,28 @@
-import "./global.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="nav-logo">Portfolio</div>
 
-      <ul className="nav-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About me</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#photography">Photography</a></li>
-        <li><a href="#blogg">Blogg</a></li>
-        <li><a href="#contact">Contact</a></li>
+      {/* Hamburger Icon */}
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <span className={open ? "bar open" : "bar"}></span>
+        <span className={open ? "bar open" : "bar"}></span>
+        <span className={open ? "bar open" : "bar"}></span>
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className={open ? "nav-links mobile-open" : "nav-links"}>
+        <li><Link to="/" onClick={() => setOpen(false)}>Home</Link></li>
+        <li><Link to="/about" onClick={() => setOpen(false)}>About me</Link></li>
+        <li><Link to="/projects" onClick={() => setOpen(false)}>Projects</Link></li>
+        <li><Link to="/photography" onClick={() => setOpen(false)}>Photography</Link></li>
+        <li><Link to="/blogg" onClick={() => setOpen(false)}>Blogg</Link></li>
+        <li><Link to="/contact" onClick={() => setOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   );

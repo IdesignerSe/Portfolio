fae72cec470c85ea8068253c43c7fb817f4b4f85
idesignerSe <hebera75@gmail.com>
2026-06-17 12:49:41 +0200

    buttoms detail pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 import bloggData from "../data/blogg.json";
 import projectsData from "../data/projects.json";
 import photographyData from "../data/photography_projects.json";
@@ -29,7 +31,11 @@ export default function Home() {
           <p className="hero-text">
             Insights, experiences, and updates from my work in development and photography.
           </p>
-          <button className="hero-btn">Read More</button>
+
+          {/* FIXED: Read More navigates to blog detail */}
+          <Link to={`/blogg/${latestBlog.id}`} className="hero-btn">
+            Read More
+          </Link>
         </div>
       </div>
 
@@ -41,7 +47,11 @@ export default function Home() {
           <img src={latestProject.image} alt={latestProject.title} />
           <h2>{latestProject.title}</h2>
           <p>{latestProject.description}</p>
-          <button className="home-btn">View Project</button>
+
+          {/* FIXED: View Project navigates to project detail */}
+          <Link to={`/projects/${latestProject.id}`} className="home-btn">
+            View Project
+          </Link>
         </div>
 
         {/* Latest Photography Project */}
@@ -49,7 +59,11 @@ export default function Home() {
           <img src={latestPhotoProject.image} alt={latestPhotoProject.title} />
           <h2>{latestPhotoProject.title}</h2>
           <p>{latestPhotoProject.description}</p>
-          <button className="home-btn">View Photography</button>
+
+          {/* FIXED: View Photography navigates to photography detail */}
+          <Link to={`/photography/${latestPhotoProject.id}`} className="home-btn">
+            View Photography
+          </Link>
         </div>
 
       </div>
